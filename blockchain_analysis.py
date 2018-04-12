0ddb126a2628faf7f7a1ccb3942dfa06cdac01ec
benjaminshi02003220 <346473509@qq.com>
2018-04-12 17:07:42 +0800

    comments

--- blockchain_analysis.py
+++ blockchain_analysis.py
@@ -14,12 +14,16 @@ import pyodbc
 con=pyodbc.connect('DRIVER={SQL Server};SERVER=Server;DATABASE=DB;UID=ID;PWD=pwd')
 
 def getblockinfo(a,b):
+#input the data from which block range you want to collect.
     for blocks in range(a,b):
         url="https://chain.api.btc.com/v3/block/{block}/tx".format(block=blocks)
+		#connect to btc.com
         tx= requests.request("GET", url)
         block_tx=json.loads(tx.text)
         height=block_tx['data']['list'][0]['block_height']
+		#get height
         block_time=block_tx['data']['list'][0]['block_time']
+		#get block time(timestamp)
 
         for i in block_tx['data']['list']:
             is_coinbase=i['is_coinbase']
@@ -31,6 +35,7 @@ def getblockinfo(a,b):
                 else:
                     from_address=None
                 prev_value=-from_add['prev_value']/100000000
+				#get tranfer value
                 print('height={h},time={t},add={fa},value={pv}'.format(h=height,t=block_time,fa=from_address,pv=prev_value))
                 cursor=con.cursor()
                 cursor.execute("insert into dbo.blockchain_tx values({0},{1},'{2}',{3})".format(height,block_time,from_address,prev_value)) 
